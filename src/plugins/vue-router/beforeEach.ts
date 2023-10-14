89c8a7418f717e1db2aad7c6671c9d8a42bca0dc
Luiz Humberto <luizhumberto@gmail.com>
2023-10-14 13:44:26 -0300

    feat: code auth route logic #3

--- src/plugins/vue-router/beforeEach.ts
+++ src/plugins/vue-router/beforeEach.ts
@@ -1,12 +1,29 @@
 import { NavigationGuardNext, RouteLocationNormalized } from 'vue-router'
 import { APP_TITLE } from '@/config'
-console.log('aqui só quando carrega')
+const user = true
+
 export default (
   to: RouteLocationNormalized,
   from: RouteLocationNormalized,
   next: NavigationGuardNext,
 ) => {
-  console.log('sempre passa por aqui')
   document.title = `${APP_TITLE} - ${to.meta.title}`
-  next()
+  if (!to.meta.requiresAuth) {
+    if ((to.name === 'LoginView' || to.name === 'SignupView') && user) {
+      console.log(to.name, ' Está logado, nao precisa logar')
+      next({ name: 'HomeView' })
+    } else {
+      console.log(to.name, ' nao necessita de login')
+      next()
+    }
+  } else {
+    console.log(to.name, ' Precisa de login')
+    if (!user) {
+      console.log(to.name, ' Não está logado, vai para login')
+      next({ name: 'LoginView' })
+    } else {
+      console.log(to.name, ' Esta logado')
+      next()
+    }
+  }
 }
