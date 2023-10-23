78758f396f2cf2de769a8df28e2ff7a93f9ddf3a
betogroo <luizhumberto@gmail.com>
2023-10-23 16:19:17 -0300

    refactor: move logout button to app bar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,28 @@
 <script setup lang="ts">
+import { onMounted } from 'vue'
 import { AppBar } from '@/shared/components'
-import { useRoute } from 'vue-router'
+import { useAuth } from './modules/auth/composables'
+import { useRoute, useRouter } from 'vue-router'
+const { getUser, user, logout, isPending: logoutPending } = useAuth()
 const route = useRoute()
+const router = useRouter()
+onMounted(async () => {
+  await getUser()
+})
+const handleLogout = async () => {
+  await logout().then(() => {
+    router.push({ name: 'LoginView' })
+  })
+}
 </script>
 <template>
   <v-app>
-    <AppBar v-if="!route.meta.hideAppBar" />
+    <AppBar
+      v-if="!route.meta.hideAppBar"
+      :is-pending="logoutPending"
+      :user="user"
+      @logout="handleLogout"
+    />
     <v-main>
       <Suspense>
         <RouterView />
