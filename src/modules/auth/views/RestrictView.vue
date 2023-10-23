cee15c3270d57a5efc4c05863fd3393d7d42d486
betogroo <luizhumberto@gmail.com>
2023-10-23 15:17:06 -0300

    feat: retunr user json in restrict page

--- src/modules/auth/views/RestrictView.vue
+++ src/modules/auth/views/RestrictView.vue
@@ -1,5 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useAuth } from '../composables'
+const { getUser, user } = useAuth()
+await getUser()
+</script>
 
 <template>
   <div>Restrict</div>
+  <pre v-if="user">{{ JSON.stringify(user, undefined, 2) }}</pre>
 </template>
