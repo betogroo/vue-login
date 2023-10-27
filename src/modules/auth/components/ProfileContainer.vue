9757a961473e8735921b1d986fcf6d94b4977571
betogroo <luizhumberto@gmail.com>
2023-10-27 13:44:20 -0300

    refactor: stylize components

--- src/modules/auth/components/ProfileContainer.vue
+++ src/modules/auth/components/ProfileContainer.vue
@@ -11,35 +11,36 @@ const { userProfile } = toRefs(props)
 </script>
 
 <template>
-  <v-container>
-    <h1 class="text-h5">{{ userProfile.full_name }}</h1>
+  <v-card
+    class="pa-2 ma-1"
+    variant="outlined"
+  >
+    <h1 class="text-h5 text-left">{{ userProfile.full_name }}</h1>
     <h1 class="text-h6 font-weight-light">{{ userProfile.email }}</h1>
-    <v-card>
-      <v-card-text>
-        <ProfileInfor title="Nome">{{ userProfile.full_name }}</ProfileInfor>
-        <ProfileInfor title="Email">{{ userProfile.email }}</ProfileInfor>
-        <ProfileInfor title="Nome de Usuário">{{
-          userProfile.username
-        }}</ProfileInfor>
-        <v-divider></v-divider>
-        <ProfileInfor title="Website"
-          ><v-list-item
-            class="pa-0"
-            density="compact"
-            :href="userProfile.website?.toString()"
-            nav
-            tag="a"
-            variant="plain"
-            >{{ userProfile.website }}</v-list-item
-          >
-        </ProfileInfor>
-        <ProfileInfor title="Data de Cadastro">{{
-          new Date(userProfile.created_at!).toLocaleDateString()
-        }}</ProfileInfor>
-        <ProfileInfor title="Último Login">{{
-          new Date(userProfile.last_sign_in_at!).toLocaleString()
-        }}</ProfileInfor>
-      </v-card-text>
-    </v-card>
-  </v-container>
+    <v-card-text>
+      <ProfileInfor title="Nome">{{ userProfile.full_name }}</ProfileInfor>
+      <ProfileInfor title="Email">{{ userProfile.email }}</ProfileInfor>
+      <ProfileInfor title="Nome de Usuário">{{
+        userProfile.username
+      }}</ProfileInfor>
+      <v-divider></v-divider>
+      <ProfileInfor title="Website"
+        ><v-list-item
+          class="pa-0"
+          density="compact"
+          :href="userProfile.website?.toString()"
+          nav
+          tag="a"
+          variant="plain"
+          >{{ userProfile.website }}</v-list-item
+        >
+      </ProfileInfor>
+      <ProfileInfor title="Data de Cadastro">{{
+        new Date(userProfile.created_at!).toLocaleDateString()
+      }}</ProfileInfor>
+      <ProfileInfor title="Último Login">{{
+        new Date(userProfile.last_sign_in_at!).toLocaleString()
+      }}</ProfileInfor>
+    </v-card-text>
+  </v-card>
 </template>
--- src/modules/auth/views/ProfileView.vue
+++ src/modules/auth/views/ProfileView.vue
@@ -13,21 +13,21 @@ const { getProfile, updateProfile, isPending } = useProfile()
 if (user.value) await getProfile(user.value.id)
 
 const handleSubmit = async (value: ProfileCasting) => {
-  const updates = { ...value, id: user.value!.id }
+  if (!user.value) return
+  const updates = { ...value, id: user.value.id }
   await updateProfile(updates)
 }
 </script>
 
 <template>
-  <ProfileContainer :user-profile="userProfile" />
-  <pre v-if="profile">{{ JSON.stringify(userProfile, undefined, 2) }}</pre>
-
-  <ProfileForm
-    :is-pending="isPending"
-    :profile="profile!"
-    :user="user"
-    @update-profile="(value) => handleSubmit(value)"
-  />
-
-  <v-divider></v-divider>
+  <v-container class="justify-center">
+    <ProfileContainer :user-profile="userProfile" />
+    <ProfileForm
+      :is-pending="isPending"
+      :profile="profile!"
+      :user="user"
+      @update-profile="(value) => handleSubmit(value)"
+    />
+    <v-divider></v-divider>
+  </v-container>
 </template>
