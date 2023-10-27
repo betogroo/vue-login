7bec330d514b28f724bb2bdf3cf1a62ab0f02f6b
betogroo <luizhumberto@gmail.com>
2023-10-27 13:46:59 -0300

    feat: add reset field in profile form

--- src/modules/auth/components/ProfileForm.vue
+++ src/modules/auth/components/ProfileForm.vue
@@ -15,10 +15,11 @@ const emit = defineEmits<{
   updateProfile: [values: ProfileCasting]
 }>()
 
-const { values, handleSubmit, meta } = useForm<ProfileCasting>({
-  validationSchema: validationProfileSchema,
-  initialValues: props.profile,
-})
+const { values, handleSubmit, meta, handleReset, resetField } =
+  useForm<ProfileCasting>({
+    validationSchema: validationProfileSchema,
+    initialValues: props.profile,
+  })
 
 const username = useField('username', validationProfileSchema)
 const website = useField('website', validationProfileSchema)
@@ -32,46 +33,72 @@ const onSubmit = handleSubmit(async () => {
 </script>
 
 <template>
-  <v-form @submit.prevent="onSubmit">
-    <v-row>
-      <v-col cols="12">
-        <v-text-field
-          v-model="username.value.value"
-          dirty
-          :error-messages="username.errorMessage.value"
-        />
-      </v-col>
-      <v-col cols="12">
-        <v-text-field
-          v-model="website.value.value"
-          dirty
-          :error-messages="website.errorMessage.value"
-        />
-      </v-col>
-      <v-col cols="12">
-        <v-text-field
-          v-model="avatar_url.value.value"
-          dirty
-          :error-messages="avatar_url.errorMessage.value"
-        />
-      </v-col>
-      <v-col cols="12">
-        <v-text-field
-          v-model="full_name.value.value"
-          dirty
-          :error-messages="full_name.errorMessage.value"
-        />
-      </v-col>
-      <v-col>
-        <v-btn
-          block
-          color="primary"
-          :disabled="!meta.valid"
-          :loading="isPending"
-          type="submit"
-          >Atualizar</v-btn
-        >
-      </v-col>
-    </v-row>
-  </v-form>
+  <v-card
+    class="pa-2 ma-1"
+    variant="outlined"
+  >
+    <v-card-text>
+      <v-form @submit.prevent="onSubmit">
+        <v-row no-gutters>
+          <v-col cols="12">
+            <v-text-field
+              v-model="username.value.value"
+              clearable
+              dirty
+              :error-messages="username.errorMessage.value"
+              label="Nome de Usuário"
+              @click:clear="resetField('username')"
+            />
+          </v-col>
+          <v-col cols="12">
+            <v-text-field
+              v-model="full_name.value.value"
+              clearable
+              dirty
+              :error-messages="full_name.errorMessage.value"
+              label="Nome Completo"
+              @click:clear="resetField('full_name')"
+            />
+          </v-col>
+          <v-col cols="12">
+            <v-text-field
+              v-model="website.value.value"
+              clearable
+              dirty
+              :error-messages="website.errorMessage.value"
+              label="Website"
+              @click:clear="resetField('website')"
+            />
+          </v-col>
+          <v-col cols="12">
+            <v-text-field
+              v-model="avatar_url.value.value"
+              clearable
+              dirty
+              :error-messages="avatar_url.errorMessage.value"
+              label="URL do Avatar"
+              @click:clear="resetField('avatar_url')"
+            />
+          </v-col>
+          <v-col class="d-flex justify-end">
+            <v-btn
+              class="mr-3"
+              color="red"
+              :disabled="!meta.valid"
+              :loading="isPending"
+              type="submit"
+              >Atualizar</v-btn
+            >
+            <v-btn
+              color="primary"
+              :disabled="!meta.dirty"
+              @click="handleReset"
+              >Redefinir Tudo</v-btn
+            >
+          </v-col>
+        </v-row>
+      </v-form>
+      <pre>{{ meta }}</pre>
+    </v-card-text>
+  </v-card>
 </template>
