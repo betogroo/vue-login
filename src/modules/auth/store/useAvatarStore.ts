f06830a9b753e9bcc628724d3f870399300b92f5
betogroo <luizhumberto@gmail.com>
2023-11-14 09:54:34 -0300

    feat: upload img to buckets

--- src/modules/auth/store/useAvatarStore.ts
+++ src/modules/auth/store/useAvatarStore.ts
@@ -2,6 +2,8 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
 export const useAvatarStore = defineStore('avatar', () => {
+  const editMode = ref(false)
+  const file = ref<File | null>(null)
   const src = ref<string | undefined>('ddd')
-  return { src }
+  return { src, editMode, file }
 })
--- src/modules/auth/views/ProfileView.vue
+++ src/modules/auth/views/ProfileView.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import { supabase } from '@/plugins/supabase'
+import { useHelpers } from '@/shared/composables'
+const { delay } = useHelpers()
 import { ref } from 'vue'
 import { AppFileBtn } from '@/shared/components/'
 import {
@@ -14,14 +17,13 @@ import { useProfileStore } from '../store/useProfileStore'
 import { useAvatarStore } from '../store/useAvatarStore'
 import { useProfile } from '../composables'
 import { Profile } from '../types/Profile'
-import { render } from 'vue'
 const store = useAuthStore()
 const profileStore = useProfileStore()
 const avatarStore = useAvatarStore()
 const profileForm = ref(false)
 const { user } = storeToRefs(store)
+const { file } = storeToRefs(avatarStore)
 const { profile, userProfile } = storeToRefs(profileStore)
-const { src } = storeToRefs(avatarStore)
 const { getProfile, updateProfile, isPending, error } = useProfile()
 
 if (user.value) await getProfile(user.value.id)
@@ -42,13 +44,38 @@ const toggleForm = () => {
 }
 const handleFile = (evt: Event) => {
   const input = evt.target as HTMLInputElement
-  const file = input.files?.length ? input.files[0] : null
-  if (file) {
+  avatarStore.file = input.files?.length ? input.files[0] : null
+  if (avatarStore.file) {
     const reader = new FileReader()
     reader.onload = (e) => {
       avatarStore.src = e.target?.result as string
     }
-    reader.readAsDataURL(file)
+    reader.readAsDataURL(avatarStore.file)
+    avatarStore.editMode = true
+  }
+}
+
+const updateAvatar = async () => {
+  console.log('Vai salvar a parada')
+  try {
+    error.value = null
+    isPending.value = true
+    const fileExt = file.value?.name.split('.').pop()
+    const filePath = `${Math.random()}.${fileExt}`
+    if (file.value) {
+      const { error: err } = await supabase.storage
+        .from('avatars')
+        .upload(filePath, file.value)
+      if (err) throw err
+    }
+    await delay()
+    avatarStore.editMode = false
+  } catch (err) {
+    const e = err as Error
+    error.value = e.message
+    console.log(e)
+  } finally {
+    isPending.value = false
   }
 }
 </script>
@@ -60,9 +87,16 @@ const handleFile = (evt: Event) => {
       :user-profile="userProfile"
     />
     <AppFileBtn
+      v-if="!avatarStore.editMode"
       text="Alterar"
       @change="handleFile"
       >Alterar</AppFileBtn
+    >
+    <v-btn
+      v-else
+      :loading="isPending"
+      @click="updateAvatar"
+      >Salvar</v-btn
     >
     <ProfileHead
       :user-profile="userProfile"
