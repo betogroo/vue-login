e9ce4317cbfd0a12a850191b7f7508435d2c2f8e
Luiz Humberto <luizhumberto@gmail.com>
2023-12-05 16:45:01 -0300

    feat: create feedback store

---
+++ src/shared/store/useFeedbackStore.ts
@@ -0,0 +1,18 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import { useHelpers } from '../composables'
+
+const { delay: _delay } = useHelpers()
+
+export const useFeedbackStore = defineStore('feedback', () => {
+  const isPending = ref<string | boolean>(false)
+  const error = ref<Error | null | string>(null)
+
+  const clearErrorAndSetPending = async (action: string, delay = false) => {
+    error.value = null
+    isPending.value = action
+    if (delay) await _delay()
+  }
+
+  return { isPending, error, clearErrorAndSetPending }
+})
