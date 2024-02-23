c97efcbdc63582d0ebb1f10835b1b880255de574
betogroo <luizhumberto@gmail.com>
2024-02-23 08:56:56 -0300

    fix: reset all state on logout #63

--- src/modules/auth/composables/useAuth.ts
+++ src/modules/auth/composables/useAuth.ts
@@ -3,12 +3,16 @@ import { Credentials } from '../types/Auth'
 import { useHelpers } from '@/shared/composables'
 import { useAuthStore } from '../store/useAuthStore'
 import { useFeedbackStore } from '@/shared/store/useFeedbackStore'
+import { useProfileStore } from '../store/useProfileStore'
+import { useAvatarStore } from '../store/useAvatarStore'
 import { storeToRefs } from 'pinia'
 const { setDefaultUsername, handleError } = useHelpers()
 
 const useAuth = () => {
   const store = useAuthStore()
   const feedbackStore = useFeedbackStore()
+  const profileStore = useProfileStore()
+  const avatarStore = useAvatarStore()
   const { error, isPending } = storeToRefs(feedbackStore)
   const signup = async (credentials: Credentials) => {
     try {
@@ -57,6 +61,8 @@ const useAuth = () => {
       await feedbackStore.clearErrorAndSetPending('logout', true)
       const { error: err } = await supabase.auth.signOut()
       store.user = null
+      profileStore.$reset()
+      avatarStore.$reset()
       if (err) throw err
     } catch (err) {
       feedbackStore.error = handleError(err)
