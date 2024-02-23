7bf790be63b40f944b391dedb55b662f869c370f
betogroo <luizhumberto@gmail.com>
2024-02-23 08:56:23 -0300

    feat(useAvatarStore): create reset store

--- src/modules/auth/store/useAvatarStore.ts
+++ src/modules/auth/store/useAvatarStore.ts
@@ -5,5 +5,11 @@ export const useAvatarStore = defineStore('avatar', () => {
   const editMode = ref(false)
   const file = ref<File | null>(null)
   const src = ref<string | undefined>('ddd')
-  return { src, editMode, file }
+
+  const $reset = () => {
+    editMode.value = false
+    file.value = null
+    src.value = undefined
+  }
+  return { src, editMode, file, $reset }
 })
